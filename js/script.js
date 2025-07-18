d67cf05eaad69c8e6a4b3535eecea34bfc414cae
Jasmine A. Santoso <jasminesantoso06@gmail.com>
2025-07-18 15:59:41 +0800

    RevoU Coding Camp

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+welcomepopup();
+
+function welcomepopup(){
+  let userName = prompt("Please enter your name:");
+  if (userName != ''){
+    document.getElementById('welcome-user').innerHTML = userName;
+  }
+}
+
+function validateForm() {
+ const nameInput = document.getElementById('name-input');
+
+ if (nameInput.value === '') {
+   alert('Please enter your name.');
+   
+ } else {
+    document.getElementById('message-output').innerHTML = `Thank you, ${nameInput.value} for your message!`;
+    nameInput.value = ''; // Clear the input field after submission
+ }
+}
